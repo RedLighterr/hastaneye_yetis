d93685651bf8ec9c9845ebd936e7a48a9d003242
Muhammed Furkan Yüce <89073406+RedLighterr@users.noreply.github.com>
2023-08-12 15:27:22 +0300

    .

--- Speedy Drag/Assets/Scripts/MenuManager.cs
+++ Speedy Drag/Assets/Scripts/MenuManager.cs
@@ -5,12 +5,19 @@ public class MenuManager : MonoBehaviour
 {
     [Header("Panels")]
     [SerializeField] GameObject levelSelection;
+    [SerializeField] GameObject credits;
     [SerializeField] GameObject Buttons;
 
     public void BaslaGeriButton()
     {
         Buttons.SetActive(!Buttons.activeSelf);
         levelSelection.SetActive(!levelSelection.activeSelf);
+    }
+
+    public void CreditsGeriButton()
+    {
+        Buttons.SetActive(!Buttons.activeSelf);
+        credits.SetActive(!credits.activeSelf);
     }
 
     public void NextButton(int levelId = 1)
